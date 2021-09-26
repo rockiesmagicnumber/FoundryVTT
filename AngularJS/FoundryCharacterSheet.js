328f1ccd7fa182306d762f0658c5765261ce2984
Andrew Martin <rockiesmagicnumber@gmail.com>
2021-09-26 10:11:39 -0600

    i have no idea what I'm doing - project standup suuuuucks

---
+++ AngularJS/FoundryCharacterSheet.js
@@ -0,0 +1,9 @@
+const angular = require("angular");
+
+var app = angular.module('CharacterSheetApp', []);
+
+app.controller('CharacterSheetController', CharacterSheetController);
+
+var CharacterSheetController = () => {
+    var vm = this;
+};
