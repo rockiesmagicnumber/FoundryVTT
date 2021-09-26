bb101eadc5fff73705acfa4a5aa5c3c3f83c521f
Andrew Martin <rockiesmagicnumber@gmail.com>
2021-09-26 11:31:23 -0600

    trying this whole stupid thing again in Sublime Text

--- AngularJS/FoundryCharacterSheet.js
+++
@@ -1,9 +0,0 @@
-const angular = require("angular");
-
-var app = angular.module('CharacterSheetApp', []);
-
-app.controller('CharacterSheetController', CharacterSheetController);
-
-var CharacterSheetController = () => {
-    var vm = this;
-};
